b80e2dd95d7b53a6e3c2866d3970103976f7def6
Martin Ramirez <jomarb13@hotmail.com>
2019-01-10 03:37:17 +0000

    Deleting spaces and whitespaces errors

--- app/core/models.py
+++ app/core/models.py
@@ -5,13 +5,13 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,\
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password = None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Creates a save new user"""
         if not email:
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password):
@@ -19,16 +19,15 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using = self._db)
+        user.save(using=self._db)
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email insted of username"""
-    email = models.EmailField(max_length = 255, unique = True)
-    name = models.CharField(max_length = 255)
-    is_active = models.BooleanField(default = True)
-    is_staff = models.BooleanField(default = False)
-
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,19 +6,18 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email successful"""
-        email= 'test@test.com'
-        password= 'Testpass123'
-        user= get_user_model().objects.create_user(
-            email= email,
-            password= password)
+        email = 'test@test.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalize(self):
         """Test the email for a new user is normalized"""
-        email= 'test@TEST.com'
-        user= get_user_model().objects.create_user(email, 'test123')
-
+        email = 'test@TEST.com'
+        user = get_user_model().objects.create_user(email, 'test123')
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
@@ -28,9 +27,8 @@ class ModelTests(TestCase):
 
     def test_create_new_super_user(self):
         """Test creating a new superuser"""
-        user= get_user_model().objects.create_superuser(
+        user = get_user_model().objects.create_superuser(
             'test@test.com', 'test123'
         )
-
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
